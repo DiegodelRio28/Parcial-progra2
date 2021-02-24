11decd62683af0942bf1d6d5c2ae4f36346fb51d
Diego Francisco Arévalo del Río <arevalod.28@gmail.com>
2021-02-23 21:12:08 -0600

    parcial

--- pregunta3/script.js
+++ pregunta3/script.js
@@ -1,25 +1,18 @@
-// Find a <table> element with id="myTable":
 function add() {
     var fecha = document.getElementById("fecha").value;
     var bebida = document.getElementById("bebida").value;
     var cantidad = document.getElementById("cantidad").value;
-    // if(_nom.trim()==''){
-    //     alert("Ingrese nombre del producto");
+    // if(fecha==''){
+    //     alert("Ingrese fecha");
     // }
-    // if(_cat.trim()==''){
-    //     alert("Ingrese categoria del producto");
-
+    // if(bebida==''){
+    //     alert("Ingrese bebida");
     // }
-    // if(_precio.trim()==''){
-    //     alert("Ingrese precio del producto");
+    // if(cantidad==''){
+    //     alert("Ingrese cantidad");
+    //}
 
-    // }
-    // if(_stock.trim()==''){
-    //     alert("Ingrese stock del producto");
-
-    // }
-
-    var fila = "<tr><td>" + _nom + "</td><td>" + _cat + "</td><td>" + _precio + "</td><td>" + _stock + "</td></tr>";
+    var fila = "<t;><td>" + fecha + "</td><td>" + bebida + "</td><td>" + cantidad;
 
     document.getElementById("tbl").innerHTML = fila;
 }
